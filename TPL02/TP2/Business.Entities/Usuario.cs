6a36fd791a8ab6ef3b7c1b6ee9e1b5b5d81ceaed
Francisco Mendiburu <franmendi.fm@gmail.com>
2021-05-13 12:13:05 -0300

    Add project files.

---
+++ TPL02/TP2/Business.Entities/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Usuario
+    {
+        private string _NombreUsuario, _Clave, _Nombre, _Apellido, _Email;
+        private bool _Habilitado;
+
+        public string NombreUsuario
+        {
+            get { return _NombreUsuario; }
+            set { _NombreUsuario = value; }
+        }
+        public string  Clave
+        {
+            get { return _Clave; }
+            set { _Clave = value; }
+        }
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public string  Apellido
+        {
+            get { return _Apellido; }
+            set { _Apellido = value; }
+        }
+
+        public string  Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+
+
+    }
+}
