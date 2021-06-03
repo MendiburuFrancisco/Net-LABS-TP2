e267ba0c546662c5926a1a6a0bb3b80cef4a78e8
Francisco Mendiburu <franmendi.fm@gmail.com>
2021-06-03 13:47:32 -0300

    TP2L03

---
+++ TPL03/TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+namespace Business.Logic
+{
+    class UsuarioLogic:BusinessLogic
+    {
+        
+
+            Data.Database.UsuarioAdapter _UsuarioData;
+
+            public Data.Database.UsuarioAdapter UsuarioData
+            {
+                get
+                {
+                    return _UsuarioData;
+                }
+                set
+                {
+                    _UsuarioData = value;
+                }
+            }
+
+
+            public UsuarioLogic()
+            {
+                //constructor
+            }
+
+            public void GetOne()
+            {
+
+            }
+
+            //public List<Usuario> GetAll()
+            //{
+            //return 
+            // }
+
+        public void Save()
+            {
+
+            }
+
+            public void Delete()
+            {
+
+            }
+        }
+
+
+    }
+
---
+++ TPL03/TP2/Diagrama/BusinessEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diagrama
+{
+    public class BusinessEntity
+    {
+      
+        private int _ID;
+        private States _States;
+
+        public int ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public States States
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+         
+    }
+}
